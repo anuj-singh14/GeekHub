045a5ee4738bdc214c295669697aca101e498d5e
mrpriyansh <mr_priyanshgaharana@gmail.com>
2020-10-28 12:07:53 +0530

    test4

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use('/api/auth', require('./routes/api/auth'));
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'));
     app.get('*', (req, res)=>{
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
     })
 }
 
