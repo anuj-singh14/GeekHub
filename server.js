58e2b3781b04106191fa08837526a19b83a87668
mrpriyansh <mr_priyanshgaharana@gmail.com>
2020-10-28 12:16:39 +0530

    test6

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/auth', require('./routes/api/auth'));
-app.get('testing', (req, res)=>{
+app.get('/testing', (req, res)=>{
     console.log('asa');
     res.send('TESTING');
 })
